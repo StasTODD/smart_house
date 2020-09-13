a3e105b9c72eca0a0c31b3bb48b2dca609a8bec2
StasTODD <todd.stas@gmail.com>
2020-09-14 00:12:09 +0300

    Chg: DB updateting procedure
    - Add 'def query_set_connection_status()' with universal query for 'DeviceConnectionStatus' table.
    - Add 'syslog_message_parser()' for parse Mikrotik syslog message.
    - Optimizing python modules import.
    - Refactor 'def db_updater()'.

--- lib/help_functions.py
+++ lib/help_functions.py
@@ -47,7 +47,52 @@ def query_injection(conn: type(sqlite3.connect), query_message: str, fetch: bool
         return cur.fetchall()
 
 
+def syslog_message_parser(message: str) -> Dict[str, str]:
+    """
+    :param message: 'script,info 10.201.0.10|52:12:54:35:25:21|1'
+    :return: {
+        "client_ip": "10.201.0.10",
+        "cleint_mac": "52:12:54:35:25:21",
+        "client_status": message[2],
+    }
+    """
+    message = message.split()[1].split("|")
+    return {
+        "client_ip": message[0],
+        "cleint_mac": message[1].lower(),
+        "client_status": message[2],
+    }
+
+
+def query_set_connection_status(conn: type(sqlite3.connect), client_address: str, client_status: int):
+    query = f"""
+        INSERT OR replace INTO DeviceConnectionStatus (id_device_connection_status,
+                                                       Device_id_device,
+                                                       LocationPlace_id_location_place,
+                                                       online)
+        VALUES (
+                (SELECT id_device_connection_status FROM Device
+                    LEFT JOIN DeviceConnectionStatus
+                    ON DeviceConnectionStatus.Device_id_device = Device.id_device
+                    WHERE Device.ip_address4 = {inet_aton(client_address)}),
+                (SELECT id_device FROM Device WHERE Device.ip_address4 = {inet_aton(client_address)}),
+                (SELECT id_location_place FROM LocationPlace WHERE LocationPlace.id_location_place IN
+	                (SELECT LocationPlace_id_location_place FROM RefDeviceLocationPlace WHERE Device_id_device = 
+		                (SELECT id_device FROM Device WHERE ip_address4 = {inet_aton(client_address)}))
+	                AND	LocationPlace.Place_id_place IS NULL),
+                {client_status}
+               );"""
+
+    if conn is not None:
+        query_injection(conn, query)
+        conn.commit()
+    else:
+        print("Error! cannot create the database connection.")
+
+
 __all__ = ["create_dir",
            "hash_str",
            "inet_aton",
-           "inet_ntoa"]
+           "inet_ntoa",
+           "syslog_message_parser",
+           "query_set_connection_status"]
--- syslog_catcher.py
+++ syslog_catcher.py
@@ -1,13 +1,49 @@
 import socketserver
 from datetime import datetime
 import time
+from os.path import isfile
+from typing import List, Dict, Union, Any
+
+from lib.help_functions import syslog_message_parser, query_set_connection_status
+from db import create_db, basic_data_filling
 
 
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = bytes.decode(self.request[0].strip())
         # socket = self.request[1]
-        print(self.client_address[0], [data])
+
+        syslog_server = self.client_address[0]
+        syslog_message = syslog_message_parser(data)
+        db_updater(syslog_server, syslog_message)
+
+
+def db_updater(syslog_server: str, syslog_message: Dict[str, str]):
+    """
+    Change data in DB smart_house_db.db.
+
+    :param syslog_server: '10.201.0.1'
+    :param syslog_message: {'client_ip': '10.201.0.10', 'cleint_mac': '52:12:54:35:25:21', 'client_status': '1'}
+    :return:
+    """
+    database_name = "db/smart_house_db.db"
+    if isfile(database_name):
+        conn = create_db.create_connection(database_name)
+    else:
+        create_db.main(database_name)
+        basic_data_filling.main(database_name)
+        conn = create_db.create_connection(database_name)
+
+    client_ip = syslog_message.get("client_ip")
+    cleint_mac = syslog_message.get("cleint_mac")
+    client_status = syslog_message.get("client_status")
+
+    # Set dhcp-server status - active:
+    query_set_connection_status(conn, syslog_server, 1)
+    # Set connected device status - active:
+    query_set_connection_status(conn, client_ip, int(client_status))
+
+    conn.close()
 
 
 def main():
